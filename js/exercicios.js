be07c49dfa4b6e87498840207e6fdf55bdb2a3aa
brunomoraesferreira <89628890+brunomoraesferreira@users.noreply.github.com>
2021-10-11 22:22:23 -0300

    111021.2222

--- js/exercicios.js
+++ js/exercicios.js
@@ -15,9 +15,26 @@ function somarDoisNumeros() {
     window.alert("A soma dos valores é " + soma);
     return;
 }
-
-const converterNumEmTexto = {
+//exercicio-004
+const numTex = {
     numA: 17,
     numB: 1.8
 };
-document.getElementById("exercicio-004").innerHTML = String(converterNumEmTexto.numA)+5+converterNumEmTexto.numB.toString();
+document.getElementById("exercicio-004").innerHTML = String(numTex.numA)+5+numTex.numB.toString();
+//exercicio-005
+const conc = {
+    pal: 'palavra',
+    num: 1981, 
+    nome: 'Bruno',
+    mon: 17805.5
+};
+document.getElementById("exercicio-005").innerHTML = `concatenando: ${conc.pal} com ${conc.num}`;
+//exercicio-006
+
+document.getElementById("exercicio-006").innerHTML = `Quantas letras tem o nome ${conc.nome}: ${conc.nome.length}<br>
+                                                      Escrever o nome ${conc.nome} em caixa alta: ${conc.nome.toUpperCase()}<br>
+                                                      Escrever o nome ${conc.nome} em caixa baixa: ${conc.nome.toLowerCase()}`;
+
+document.getElementById("exercicio-007").innerHTML = `Formatar o número ${conc.mon}<br> 
+                                                      A primeira formatação ${conc.mon.toFixed(2).replace('.',',')}<br>
+                                                      A segunda formatação: ${conc.mon.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`;
