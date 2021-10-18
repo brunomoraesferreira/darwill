0c3db3ad52c270f4e6e18aa72ab32cb60881f308
brunomoraesferreira <89628890+brunomoraesferreira@users.noreply.github.com>
2021-10-17 21:01:15 -0300

    171021.2100

--- js/exercicios.js
+++ js/exercicios.js
@@ -1,13 +1,13 @@
-//exercicio-001
-document.getElementById("exercicio-001").innerHTML = "Ola Mundo!";
-document.getElementById("exercicio-001").style.color = "red";
-//exercicio-002
+//atividade-001
+document.getElementById("atividade-001").innerHTML = "Ola Mundo!";
+document.getElementById("atividade-001").style.color = "red";
+//atividade-002
 function pedirMostrarNome() {
     var x = window.prompt("Qual é o seu nome?");
     var y = window.alert("O seu nome é " + x);
     return;
 }
-//exercicio-003
+//atividade-003
 function somarDoisNumeros() {
     var x = Number(window.prompt("Digite um número: ")); 
     var y = Number(window.prompt("Digite outro número: "));
@@ -15,29 +15,29 @@ function somarDoisNumeros() {
     window.alert("A soma dos valores é " + soma);
     return;
 }
-//exercicio-004
+//atividade-004
 const numTex = {
     numA: 17,
     numB: 1.8
 };
-document.getElementById("exercicio-004").innerHTML = String(numTex.numA)+5+numTex.numB.toString();
-//exercicio-005
+document.getElementById("atividade-004").innerHTML = String(numTex.numA)+5+numTex.numB.toString();
+//atividade-005
 const conc = {
     pal: 'palavra',
     num: 1981, 
     nome: 'Bruno',
     mon: 17805.5
 };
-document.getElementById("exercicio-005").innerHTML = `concatenando: ${conc.pal} com ${conc.num}`;
-//exercicio-006
-document.getElementById("exercicio-006").innerHTML = `Quantas letras tem o nome ${conc.nome}: ${conc.nome.length}<br>
+document.getElementById("atividade-005").innerHTML = `concatenando: ${conc.pal} com ${conc.num}`;
+//atividade-006
+document.getElementById("atividade-006").innerHTML = `Quantas letras tem o nome ${conc.nome}: ${conc.nome.length}<br>
                                                       Escrever o nome ${conc.nome} em caixa alta: ${conc.nome.toUpperCase()}<br>
                                                       Escrever o nome ${conc.nome} em caixa baixa: ${conc.nome.toLowerCase()}`;
-//exercicio-007
-document.getElementById("exercicio-007").innerHTML = `Formatar o número ${conc.mon}<br> 
+//atividade-007
+document.getElementById("atividade-007").innerHTML = `Formatar o número ${conc.mon}<br> 
                                                       A primeira formatação ${conc.mon.toFixed(2).replace('.',',')}<br>
                                                       A segunda formatação: ${conc.mon.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`;
-//exercicio-006
+//atividade-006
 var v1 = 1981;
 var v2 = 'brasília';
 var v3 = ['a', 'Banana', 1];
