dccac62e22fca01f946d481edcd9841fd0b454ed
brunomoraesferreira <89628890+brunomoraesferreira@users.noreply.github.com>
2022-01-23 13:00:42 -0300

    230122.1300

--- js/aprendizado.js
+++ js/aprendizado.js
@@ -1,20 +1,20 @@
 (function() {
   
-  var slideContainer = $('.deslizante-aprendizado');
+  var slideContainer = $('#deslizante-aprendizado');
   
   slideContainer.slick();
   
   $('.card__imagem img').hide();
-  $('.slick-active').find('.card img').fadeIn(200);
+  $('.slick-active').find('.card__involucro img').fadeIn(200);
   
   // On before slide change
   slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    $('.slick-active').find('.card img').fadeOut(1000);
+    $('.slick-active').find('.card__involucro img').fadeOut(1000);
   });
   
   // On after slide change
   slideContainer.on('afterChange', function(event, slick, currentSlide) {
-    $('.slick-active').find('.card img').fadeIn(200);
+    $('.slick-active').find('.card__involucro img').fadeIn(200);
   });
   
 })();
