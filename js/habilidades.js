64ed24bf3d61441cff6d8fa8d6ff0b40987a4703
brunomoraesferreira <89628890+brunomoraesferreira@users.noreply.github.com>
2022-02-03 16:40:25 -0300

    030222.1640

--- js/habilidades.js
+++ js/habilidades.js
@@ -2,15 +2,14 @@ var element = document.getElementById("deslizante-aprendizado");
 
 function myFunction(x) {
   if (x.matches) { // If media query matches
-   element.classList.add("midiaMaior");
-   element.classList.remove("midiaMenor");
+   element.classList.add("slider");
+   
   } else {
-   element.classList.add("midiaMenor");
-   element.classList.remove("midiaMaior");
+   element.classList.remove("slider");
 
   }
 }
 
-var x = window.matchMedia("(min-width: 768px)")
+var x = window.matchMedia("(max-width: 768px)")
 myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+x.addEventListener(myFunction) // Attach listener function on state changes
