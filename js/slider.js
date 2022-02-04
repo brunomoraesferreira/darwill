89bc1691010c40f615263c9ee0abdbf8a3e3b682
brunomoraesferreira <89628890+brunomoraesferreira@users.noreply.github.com>
2022-02-03 22:12:20 -0300

    030222.2212

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+      /*$('.slider').slick();*/
+      $(document).ready(function () {
+        $('.slider').slick({
+          accessibility: true, /*navegação por tabulação e tecla de seta*/
+          autoplay: true,      /*reprodução automática*/
+          autoplaySpeed: 3000  /*Velocidade dA reprodução automática em milissegundos, o padrão é 3000*/ 
+        });
+      });
