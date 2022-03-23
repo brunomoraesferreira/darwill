9426439f9d57981a8f2385f0e1fd9d7db7f218ef
brunomoraesferreira <89628890+brunomoraesferreira@users.noreply.github.com>
2022-03-23 18:56:56 -0300

    230222.1856

---
+++ js/deslizante.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+      accessibility: true, /*navegação por tabulação e tecla de seta*/
+      autoplay: true,      /*reprodução automática*/
+      autoplaySpeed: 3000  /*Velocidade dA reprodução automática em milissegundos, o padrão é 3000*/ 
+    });
+  });
--- js/scrollify.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $.scrollify({
-      section : ".secao",
-      offset : -53, 
-      scrollbars : false , 
-    });
-  });
